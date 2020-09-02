cdd83f483c6ac36ebd35eda779c5c809de8d4bf3
shokiogawa <syoki2578@outlook.jp>
2020-09-02 23:02:19 +0900

    firstgit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <product-list v-bind:products="products"></product-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import productList from './components/product-list.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'product-list' : productList
+  },
+  data: function(){
+    return {
+      products: [
+        {id: 1,name: 'Michael<br>スマホケース', price: 1580, image: require('./assets/images/9125.jpg'), delv: 0, isSale: true},
+        {id: 2,name: 'Raphel<br>スマホケース',price: 1580, image: require('./assets/images/9131.jpg'), delv: 0, isSale: true},
+        {id: 3,name: 'Gabriel<br>スマホケース',price: 1580, image: require('./assets/images/9800.jpg'), delv: 240, isSale: true},
+        {id: 4,name: 'Uriel<br>スマホケース',price: 980, image: require('./assets/images/9909.jpg'), delv: 0, isSale:true},
+        {id: 5,name: 'Ariel<br>スマホケース', price: 980, image: require('./assets/images/9913.jpg'), delv: 0, isSale:false},
+        {id: 6,name: 'Azrael<br>スマホケース',price: 1580, image: require('./assets/images/10648.jpg'), delv: 0,isSale:false},
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background :#000;
+  color:#fff;
 }
 </style>
